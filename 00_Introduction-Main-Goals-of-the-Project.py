657ec71e63fbfa87cf2c1fe66e0f3567e2bb19c2
isha2825 <64250083+isha2825@users.noreply.github.com>
2020-09-25 19:23:38 +0000

    Add files via upload

---
+++ 00_Introduction-Main-Goals-of-the-Project.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# ### Tasks
+
+# - Face Detection
+# - Eyes Detection
+# - Face & Eyes Detection
+# - Pedestrians Detection
+# - Cars Detection
+# - Car Plate Detection
+
+# You can Run the cell by:
+# - clicking on the Run button
+# - Shift + Enter (<b>Windows</b>).
+# - Control + Enter (<b>Mac</b>)
+
+# When we have finished with our Jupyter Notebook we go to:
+
+# <b>File</b> -> <b>Save and Checkpoint</b> and then
+
+# <b>File</b> -> <b>Close and Halt</b>
+
+# ### How to Tune Cascade Classifier
+
+# (https://docs.opencv.org/2.4/modules/objdetect/doc/cascade_classification.html#cascadeclassifier-detectmultiscale)
+# 
+# <b>detectMultiscale</b>(input image, <b>Scale Factor</b>, <b>Min Neighbors</b>)
+# - <b>Scale Factor</b> Parameter specifying how much yhe image size is reduced at each image scale.
+# 
+# 
+# - <b>Min Neighbors</b> Parameter specifying how many neighbors each candidate rectangle should have to retain it.
+
+# In[ ]:
+
+
+
+
